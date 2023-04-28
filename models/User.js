607c80ccad1c1d18f5ac7b7da830b6b0e91433b6
James Xiong <jxiong783@gmail.com>
2023-04-27 17:14:10 -0700

    initial commit

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, unique: true, required: true, trimmed: true },
+    email: { type: String, required: true, unique: true, match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,10})+$/ },
+    thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
